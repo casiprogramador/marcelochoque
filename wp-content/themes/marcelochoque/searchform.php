f2462c787c008585e536663208246bf9c6614217
pmarce <pablo_marce@hotmail.com>
2013-05-31 13:49:48 -0400

    Inicio del repositorio

---
+++ wp-content/themes/marcelochoque/searchform.php
@@ -0,0 +1,9 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <div>
+
+        <input type="search" value="" name="s" id="s" class="txt" placeholder="¿Qué buscas?">
+<!--         <img src="<?php bloginfo( 'template_url' ); ?>/img/search.png" alt=""> -->
+        <input type="submit" id="searchsubmit" value="Search" />
+
+    </div>
+</form>
